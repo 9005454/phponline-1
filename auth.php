ac1f1f68ed0175f06759d15b629d9f0c8fc5c15b
Dmitriy <shkatov.dmitry@gmail.com>
2015-10-19 21:35:43 +0500

    Add profile

--- auth.php
+++ auth.php
@@ -1,11 +1,8 @@
 <?php
 require_once(__DIR__ . '\lib\prolog.php');
-require_once(__DIR__ . '\templates\header.php');
 
-$user  = "shkatov.dmitry@gmail.com";
-$sHash =  "46f94c8de14fb36680850768ff1b7f2a";
-$data  = [];
-
+$errors = [];
+$data  	= [];
 $bValidate = true;
 $errors['email'] = false;
 $errors['passw'] = false;
@@ -14,7 +11,6 @@ if(isset($_POST['auth'])){
 	
 	$sEmail = $_POST['email'];
 	$sPassw = $_POST['passw'];
-	$errors = [];
 
 	if (!$sEmail) {
 
@@ -26,15 +22,32 @@ if(isset($_POST['auth'])){
 		$errors['passw']  = 'Не заполнено!';
 		$bValidate = false;
 	}
+	
+	$user = User::getByEmail($sEmail);
 
-	if($bValidate && $user === $sEmail && Helpers::checkPassw($sPassw, $sHash)) {
+	if (!$user) {
+		
+		$errors['email']  = 'Пользователеь с таким email не найден!';
+		$bValidate = false;
+	} 
+
+	if($bValidate && $user['email'] === $sEmail && Helpers::checkPassw($sPassw, $user['hash'])) {
 
 		echo "Вы прошли авторизацию";
+		session_start();
+		$_SESSION['user_id'] = $user['id'];
+
+	
+	} else {
+
+		$errors['passw']  = 'Не верный пароль';
 	}
 }
 
 $data = [
-		'errors' => $errors
+		'errors' => $errors,
+		'email' => $_POST['email'],
+		'passw' => $_POST['passw'],
 ];
 
 Helpers::render(__DIR__ . '\view\auth-form.php', $data);
